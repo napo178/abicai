284b652e04a186af4debfc4b5f3ecabb2b2f88f7
NapoleonPerez <87319901+napo178@users.noreply.github.com>
2022-05-20 14:09:04 -0600

    abicai

---
+++ predict.py
@@ -0,0 +1,23 @@
+
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import numpy as np
+
+
+def load_model():
+    # Portuguese to English translator
+    order_model = pickle.load(open('model/model.sav', 'rb'))
+    return order_model
+
+def run(model):
+    data = np.load("./data/train.csv")
+    X_test = data['arr_0']
+
+    result = {"data": model.predict(X_test)}
+    return result
+
+# if     __name__ == "__main__":
+#     result = run(load_model())
+#     print(result)
